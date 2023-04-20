18cb312e75f41da02f07cd3d5516772e43db7e9b
Maksym Kintor <kintormaksim@gmail.com>
2023-04-20 08:25:46 +0200

    generate drinks befault and by letter

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const dogURL = 'https://dog-api.kinduff.com/api/facts';
-const cocktailURLbyLetter = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f='; //input cocktail letter
+const cocktailURLbyLetter = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?'; //input cocktail letter
 
 
 $(document).ready(function() {
 	loadDogFact();
+	loadDrink();
 });
 
 let countOfDogs = 1;
+
 function loadDogFact(){
 	$.ajax({
 		url: dogURL,
@@ -35,6 +37,101 @@ function loadDogFact(){
 			$("#dogFactsTable").css('opacity', 0);
 			$("#dogFactsTable").append(tr);
 			$("#dogFactsTable").animate({ opacity: '1' });
+		},
+		error: function(xhr, status, error) {
+			console.error(status, error);
+		}
+	});
+}
+
+function loadDrink(){
+
+	$.ajax({
+		url: cocktailURLbyLetter+'f=a',
+		type: "GET",
+		crossDomain: true,
+		success: function(data) {
+			console.log(data);
+
+			/*
+			<tr>
+					<th scope="row">1</th>
+					<td>a</td>
+					<td>a</td>
+					<td>a</td>
+				</tr>
+			*/
+			for(var i = 0; i < 10; i ++){
+				let tr = $('<tr></tr>');
+
+				let th = $('<th  scope="row"></th>').text(data.drinks[i].idDrink);
+				let td1 = $('<td></td>').text(data.drinks[i].strDrink);
+				let td2 = $('<td></td>').text(data.drinks[i].strCategory);
+				let td3 = $('<td></td>').text(data.drinks[i].strGlass);
+				
+				tr.append(th);
+				tr.append(td1);
+				tr.append(td2);
+				tr.append(td3);
+
+
+
+				$("#drinks").css('opacity', 0);
+				$("#drinks").append(tr);
+				$("#drinks").animate({ opacity: '1' });
+			}
+
+			
+
+		},
+		error: function(xhr, status, error) {
+			console.error(status, error);
+		}
+	});
+}
+
+function loadDrinkByLetter(){
+	var letter = $("#inputLetter").val();
+	console.log("letter "+letter);
+	$.ajax({
+		url: cocktailURLbyLetter+'f='+letter,
+		type: "GET",
+		crossDomain: true,
+		success: function(data) {
+			console.log(data);
+
+			/*
+			<tr>
+					<th scope="row">1</th>
+					<td>a</td>
+					<td>a</td>
+					<td>a</td>
+				</tr>
+			*/
+			$("#drinks").css('opacity', 0);
+			for(var i = 0; i < data.drinks.length; i++){
+				let tr = $('<tr></tr>');
+
+				let th = $('<th  scope="row"></th>').text(data.drinks[i].idDrink);
+				let td1 = $('<td></td>').text(data.drinks[i].strDrink);
+				let td2 = $('<td></td>').text(data.drinks[i].strCategory);
+				let td3 = $('<td></td>').text(data.drinks[i].strGlass);
+				
+				tr.append(th);
+				tr.append(td1);
+				tr.append(td2);
+				tr.append(td3);
+
+
+
+				
+				$("#drinks").append(tr);
+				
+			}
+			$("#drinks").animate({ opacity: '1' });
+
+			
+
 		},
 		error: function(xhr, status, error) {
 			console.error(status, error);
